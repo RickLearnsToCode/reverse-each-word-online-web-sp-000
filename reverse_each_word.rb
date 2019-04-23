9f176d8b9027390a5460b59ff679a73da628b737
Rick Rosetti <rick.rosetti@gmail.com>
2019-04-23 13:43:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   words = string.split(" ")
-  output_array = [] 
+  output_array = []
   words.each {|word| output_array << word.reverse}
   return output_array.join(" ")
 end
